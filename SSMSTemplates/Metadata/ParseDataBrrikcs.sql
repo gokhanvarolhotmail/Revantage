012bee2903419f5fb15862f7ad5027376c185eec
gokhanvarolhotmail <67670356+gokhanvarolhotmail@users.noreply.github.com>
2021-08-21 14:25:08 -0400

    gv

--- SSMSTemplates/Metadata/ParseDataBrrikcs.sql
+++ SSMSTemplates/Metadata/ParseDataBrrikcs.sql
@@ -29,6 +29,33 @@ CROSS APPLY [Util].[dbo].ParseDelimitedColumns32(REPLACE(REPLACE([t].[Field], 'C
 CROSS APPLY [Util].[dbo].ParseDelimitedColumns32(SUBSTRING([t3].[Field], 2, LEN([t3].[Field]) - 2), '` ') AS [cn]
 WHERE [t].[Field] LIKE 'CREATE TABLE %' ;
 GO
+DROP TABLE IF EXISTS [dbo].[Views] ;
+GO
+SELECT
+    [k].[DatabaseName]
+  , [k].[ViewName]
+  , CONCAT(CAST(NULL AS NVARCHAR(MAX)), 'CREATE OR ALTER VIEW ', QUOTENAME([k].[DatabaseName]), '.', QUOTENAME([k].[ViewName]), '
+', STRING_AGG([k].[Field], '
+')WITHIN GROUP(ORDER BY [k].[FieldNum])) AS [Definition]
+INTO [dbo].[Views]
+FROM( SELECT
+          CAST([tn].[C1] AS NVARCHAR(128)) AS [DatabaseName]
+        , CAST([tn].[C2] AS NVARCHAR(128)) AS [ViewName]
+        , [t5].[FieldNum]
+        , [t5].[Field]
+      FROM [#temp] AS [t]
+      CROSS APPLY( SELECT TOP( 1 )* FROM [#temp] AS [t3] WHERE [t3].[FieldNum] > [t].[FieldNum] AND [t3].[Field] LIKE '--------------------------%' ORDER BY [t3].[FieldNum] ) AS [t3]
+      CROSS APPLY( SELECT TOP( 1 )* FROM [#temp] AS [t4] WHERE [t4].[FieldNum] > [t].[FieldNum] AND [t4].[Field] LIKE 'AS %' ORDER BY [t4].[FieldNum] ) AS [t4]
+      CROSS APPLY( SELECT * FROM [#temp] AS [t5] WHERE [t5].[FieldNum] >= [t4].[FieldNum] AND [t5].[FieldNum] < [t3].[FieldNum] ) AS [t5]
+      CROSS APPLY [Util].[dbo].ParseDelimitedColumns32(REPLACE(REPLACE([t].[Field], 'CREATE VIEW `', ''), '` (', ''), '`.`') AS [tn]
+      WHERE [t].[Field] LIKE 'CREATE VIEW %' ) AS [k]
+GROUP BY [k].[DatabaseName]
+       , [k].[ViewName] ;
+GO
+CREATE UNIQUE CLUSTERED INDEX [name] ON [dbo].[views]( [DatabaseName], [ViewName] ) ;
+GO
+ALTER TABLE [dbo].[Views] ADD [FQN] AS QUOTENAME([DatabaseName]) + '.' + QUOTENAME([ViewName]) ;
+GO
 DROP TABLE IF EXISTS [dbo].[Tables] ;
 GO
 SELECT
@@ -40,7 +67,9 @@ FROM [#temp2]
 GROUP BY [DatabaseName]
        , [TableName] ;
 GO
-CREATE UNIQUE CLUSTERED INDEX [name] ON [dbo].[tables]( [DatabaseName], [TableName] ) ;
+CREATE UNIQUE CLUSTERED INDEX [name] ON [dbo].[Tables]( [DatabaseName], [TableName] ) ;
+GO
+ALTER TABLE [dbo].[Tables] ADD [FQN] AS QUOTENAME([DatabaseName]) + '.' + QUOTENAME([TableName]) ;
 GO
 DROP TABLE IF EXISTS [dbo].[Columns] ;
 
@@ -48,7 +77,24 @@ SELECT *
 INTO [dbo].[Columns]
 FROM [#temp2] ;
 GO
-CREATE UNIQUE CLUSTERED INDEX [name] ON [dbo].[columns]( [DatabaseName], [TableName], [ColumnId] ) ;
+CREATE UNIQUE CLUSTERED INDEX [name] ON [dbo].[Columns]( [DatabaseName], [TableName], [ColumnId] ) ;
 GO
-CREATE UNIQUE NONCLUSTERED INDEX [coln] ON [dbo].[columns]( [DatabaseName], [TableName], [ColumnName] ) ;
+CREATE UNIQUE NONCLUSTERED INDEX [coln] ON [dbo].[Columns]( [DatabaseName], [TableName], [ColumnName] ) ;
 GO
+
+ALTER TABLE [dbo].[Columns]
+ADD
+    [FQN] AS QUOTENAME([DatabaseName]) + '.' + QUOTENAME([TableName])
+  , [SQLDataType] AS CASE WHEN [DataType] LIKE 'decimal%' THEN [DataType]
+                         WHEN [DataType] IN ('INT', 'BIGINT', 'FLOAT', 'DATE') THEN [DataType]
+                         WHEN [DataType] = 'binary' THEN 'varbinar(4000)'
+                         WHEN [DataType] = 'STRING' THEN 'NVARCHAR(4000)'
+                         WHEN [DataType] = 'DOUBLE' THEN 'REAL'
+                         WHEN [DataType] = 'TIMESTAMP' THEN 'DATETIME2(7)'
+                         WHEN [DataType] = 'BOOLEAN' THEN 'BIT'
+                         WHEN [DataType] LIKE 'ARRAY%' THEN 'NVARCHAR(MAX)'
+                     END ;
+GO
+
+SELECT * FROM [dbo].[Columns]
+GROUP BY 
